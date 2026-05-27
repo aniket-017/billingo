51742076bf3f72be88797da5c6af9b022a5c3e47
Aniket Arjun Khillare <aniketkhillare17@gmail.com>
2026-05-27 09:59:29 +0530

    Update app version to 1.0.1 and switch API base URL to production

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "Plan2Automate",
     slug: "billingo",
-    version: "1.0.0",
+    version: "1.0.1",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "billingo",
