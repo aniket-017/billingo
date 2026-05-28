6d71a7212aa3fa18f20d24979853cca8451190ac
Aniket Arjun Khillare <aniketkhillare17@gmail.com>
2026-05-28 13:10:35 +0530

    Update app version to 1.0.2

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "Plan2Automate",
     slug: "billingo",
-    version: "1.0.1",
+    version: "1.0.2",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "billingo",
